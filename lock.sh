b5aa25446068ebaba9c78e8aecbc4ccc46024a50
Maximilian von Gaisberg <github@vongaisberg.de>
2019-01-01 23:49:45 +0100

    Changed lockscreen

--- lock.sh
+++ lock.sh
@@ -2,7 +2,7 @@
 ICON=$HOME/lock.png
 TMPBG=/tmp/screen.png
 scrot /tmp/screen.png
-#convert $TMPBG -scale 10% -scale 1000% $TMPBG
-convert $TMPBG -modulate 100,30 -blur 0x8 $TMPBG
+convert $TMPBG -scale 10% -scale 1000% $TMPBG
+#convert $TMPBG -modulate 100,30 -blur 0x8 $TMPBG
 convert $TMPBG $ICON'[250x250]' -gravity center -composite -matte $TMPBG
 i3lock -u -i $TMPBG
