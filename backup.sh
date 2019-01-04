df84e0204a7cd5ba69963ed2bea76639a72cd2a7
Maximilian von Gaisberg <github@vongaisberg.de>
2019-01-04 23:41:28 +0100

    Added borg_backup script

---
+++ backup.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+##
+## Setzten von Umgebungsvariablen
+##
+
+## falls nicht der Standard SSH Key verwendet wird können
+## Sie hier den Pfad zu Ihrem private Key angeben
+# export BORG_RSH="ssh -i /home/userXY/.ssh/id_ed25519"
+
+## Damit das Passwort vom Repository nicht eingegeben werden muss
+## kann es in der Umgepungsvariable gesetzt werden
+# export BORG_PASSPHRASE="top_secret_passphrase"
+
+##
+## Setzten von Variablen
+##
+
+LOG="/var/log/borg/backup.log"
+BACKUP_USER="u194911"
+REPOSITORY_DIR="earth"
+
+## Hinweis: Für die Verwendung mit einem Backup-Account muss
+## 'your-storagebox.de' in 'your-backup.de' geändert werden.
+
+REPOSITORY="ssh://${BACKUP_USER}@${BACKUP_USER}.your-storagebox.de:23/./backups/${REPOSITORY_DIR}"
+
+##
+## Ausgabe in Logdatei schreiben
+##
+
+exec > >(tee -i ${LOG})
+exec 2>&1
+
+echo "###### Backup gestartet: $(date) ######"
+
+##
+## An dieser Stelle können verschiedene Aufgaben vor der
+## Übertragung der Dateien ausgeführt werden, wie z.B.
+##
+## - Liste der installierten Software erstellen
+## - Datenbank Dump erstellen
+##
+
+##
+## Dateien ins Repository übertragen
+## Gesichert werden hier beispielsweise die Ordner root, etc,
+## var/www und home
+## Ausserdem finden Sie hier gleich noch eine Liste Excludes,
+## die in kein Backup sollten und somit per default ausgeschlossen
+## werden.
+##
+
+echo "Übertrage Dateien ..."
+borg create -v --stats                   \
+    $REPOSITORY::'{now:%Y-%m-%d_%H:%M}'  \
+    /                                    \
+    --exclude /data                      \
+    --exclude /bin                       \
+    --exclude /boot                      \
+    --exclude /dev                       \
+    --exclude /lib                       \
+    --exclude /lib64                     \
+    --exclude /proc                      \
+    --exclude /swapfile                  \
+    --exclude /tmp                       \
+    --exclude /sbin                      \
+    --exclude /sys                       \
+    --exclude /var/run                   \
+    --exclude /run                       \
+    --exclude /lost+found                \
+    --exclude /mnt                       \
+    --exclude /var/lib/lxcfs
+
+echo "###### Backup beendet: $(date) ######"
