39b493f83d8f0e36d3eb2f8fcca878380651bf83
Maximilian von Gaisberg <github@vongaisberg.de>
2019-01-25 16:54:44 +0100

    Turning CPU power preference down when battery is low

--- power_saving/batterywarning.sh
+++ power_saving/batterywarning.sh
@@ -5,13 +5,17 @@ BATTERY_DANGER=5;
 battery=$(acpi -b | grep -Eo "[0-9]+%" | grep -Eo "[0-9]+")
 if [ "$battery" -lt "$BATTERY_WARNING" ]; then
 	if [ "$(cat ~/scripts/boot/notified)" -eq 0 ]; then
+		/home/max/git/dotfiles/power_saving/power_settings.sh 0
 		zenity --warning --title="alert" --text="Please feed me!" --no-wrap --width=150 --height=100 &
 	       	sleep 1 && i3-msg "[title=alert] focus"
 	echo 1 > ~/scripts/boot/notified 
 	fi
+else
+        echo 0 > ~/scripts/boot/notified
 fi
 if [ "$battery" -lt "$BATTERY_DANGER" ]; then
 	if [ "$(cat ~/scripts/boot/notified)" -eq 1 ]; then
+		/home/max/git/dotfiles/power_saving/power_settings.sh 0
 		zenity --error --title="alert" --text="IM STARVING!" --no-wrap --width=150 --height=100 &
 	       	sleep 1 && i3-msg "[title=alert] focus"
 
