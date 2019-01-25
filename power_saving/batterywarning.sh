82c37b5b48b146bb2f5683682db7414c4e2ca253
Maximilian von Gaisberg <github@vongaisberg.de>
2019-01-25 17:36:59 +0100

    Fixed permissions and file paths

--- power_saving/batterywarning.sh
+++ power_saving/batterywarning.sh
@@ -4,22 +4,22 @@ BATTERY_WARNING=15;
 BATTERY_DANGER=5;
 battery=$(acpi -b | grep -Eo "[0-9]+%" | grep -Eo "[0-9]+")
 if [ "$battery" -lt "$BATTERY_WARNING" ]; then
-	if [ "$(cat ~/scripts/boot/notified)" -eq 0 ]; then
-		/home/max/git/dotfiles/power_saving/power_settings.sh 0
+	if [ "$(cat ~/git/dotfiles/notified)" -eq 0 ]; then
+	sudo	/home/max/git/dotfiles/power_saving/power_settings.sh 0
 		zenity --warning --title="alert" --text="Please feed me!" --no-wrap --width=150 --height=100 &
 	       	sleep 1 && i3-msg "[title=alert] focus"
-	echo 1 > ~/scripts/boot/notified 
+	echo 1 > ~/git/dotfiles/notified 
 	fi
 else
-        echo 0 > ~/scripts/boot/notified
+        echo 0 > ~/git/dotfiles/notified
 fi
 if [ "$battery" -lt "$BATTERY_DANGER" ]; then
 	if [ "$(cat ~/scripts/boot/notified)" -eq 1 ]; then
-		/home/max/git/dotfiles/power_saving/power_settings.sh 0
+	sudo	/home/max/git/dotfiles/power_saving/power_settings.sh 0
 		zenity --error --title="alert" --text="IM STARVING!" --no-wrap --width=150 --height=100 &
 	       	sleep 1 && i3-msg "[title=alert] focus"
 
-	echo 2 > ~/scripts/boot/notified 
+	echo 2 > ~/git/dotfiles/notified 
 	fi
 fi
 
