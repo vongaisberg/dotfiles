5258fed68c5f852fd01096f2e097d501c8b6ec03
Maximilian von Gaisberg <github@vongaisberg.de>
2019-01-25 17:40:55 +0100

    Changed battery notification texts

--- power_saving/batterywarning.sh
+++ power_saving/batterywarning.sh
@@ -6,7 +6,7 @@ battery=$(acpi -b | grep -Eo "[0-9]+%" | grep -Eo "[0-9]+")
 if [ "$battery" -lt "$BATTERY_WARNING" ]; then
 	if [ "$(cat ~/git/dotfiles/notified)" -eq 0 ]; then
 	sudo	/home/max/git/dotfiles/power_saving/power_settings.sh 0
-		zenity --warning --title="alert" --text="Please feed me!" --no-wrap --width=150 --height=100 &
+		zenity --warning --title="Battery Notification" --text="Battery at 15%" --no-wrap --width=150 --height=100 &
 	       	sleep 1 && i3-msg "[title=alert] focus"
 	echo 1 > ~/git/dotfiles/notified 
 	fi
@@ -16,7 +16,7 @@ fi
 if [ "$battery" -lt "$BATTERY_DANGER" ]; then
 	if [ "$(cat ~/scripts/boot/notified)" -eq 1 ]; then
 	sudo	/home/max/git/dotfiles/power_saving/power_settings.sh 0
-		zenity --error --title="alert" --text="IM STARVING!" --no-wrap --width=150 --height=100 &
+		zenity --error --title="Battery Notification" --text="Battery at 5%" --no-wrap --width=150 --height=100 &
 	       	sleep 1 && i3-msg "[title=alert] focus"
 
 	echo 2 > ~/git/dotfiles/notified 
