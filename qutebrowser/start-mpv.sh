4f63a74a9ebe18b940ad4d904b731d92c6da25c5
Maximilian von Gaisberg <github@vongaisberg.de>
2019-02-20 18:12:20 +0100

    Added controls for MPV

---
+++ qutebrowser/start-mpv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "test"
+
+typeset -a coords
+coords=( $(/home/max/git/dotfiles/qutebrowser/get-window-coords.sh | tr ";" "\n") )
+#X Y Width Height
+
+echo "#######MPV SIZE:$2x$3+$(( $4 + ${coords[0]} ))+$(( $5 + ${coords[1]} + 19))"
+mpv --x11-name "youtube-mpv" -geometry "$2x$3+$(( $4 + ${coords[0]} ))+$(( $5 + ${coords[1]} + 19))" "$1"
+#i3-msg '[class="^mpv$"] floating enable'
