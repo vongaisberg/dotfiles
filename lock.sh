925e25e1a09b29d833e31ad48ce1b4fdf4930ec6
Maximilian von Gaisberg <github@vongaisberg.de>
2019-03-08 20:16:28 +0100

    Changed lock.sh

--- lock.sh
+++ lock.sh
@@ -5,4 +5,4 @@ scrot /tmp/screen.png
 convert $TMPBG -scale 10% -scale 1000% $TMPBG
 #convert $TMPBG -modulate 100,30 -blur 0x8 $TMPBG
 convert $TMPBG $ICON'[250x250]' -gravity center -composite -matte $TMPBG
-i3lock -u -n -i $TMPBG
+i3lock -e -n -i $TMPBG
