96546e7e26eaee4e9c8c1bf389a174ed482e4b79
Maximilian von Gaisberg <github@vongaisberg.de>
2019-03-19 20:09:45 +0100

    Battery warning will no penetrate fullscreen mode

--- power_saving/batterywarning.sh
+++ power_saving/batterywarning.sh
@@ -7,7 +7,7 @@ if [ "$battery" -lt "$BATTERY_WARNING" ]; then
 	if [ "$(cat ~/git/dotfiles/notified)" -eq 0 ]; then
 	sudo	/home/max/git/dotfiles/power_saving/power_settings.sh 0
 		zenity --warning --title="Battery Notification" --text="Battery at 15%" --no-wrap --width=150 --height=100 &
-	       	sleep 1 && i3-msg "[title=alert] focus"
+	       	sleep 1 && i3-msg "[title=Battery Notification] focus"
 	echo 1 > ~/git/dotfiles/notified 
 	fi
 else
@@ -17,7 +17,7 @@ if [ "$battery" -lt "$BATTERY_DANGER" ]; then
 	if [ "$(cat ~/scripts/boot/notified)" -eq 1 ]; then
 	sudo	/home/max/git/dotfiles/power_saving/power_settings.sh 0
 		zenity --error --title="Battery Notification" --text="Battery at 5%" --no-wrap --width=150 --height=100 &
-	       	sleep 1 && i3-msg "[title=alert] focus"
+	       	sleep 1 && i3-msg "[title=Battery Notification] focus"
 
 	echo 2 > ~/git/dotfiles/notified 
 	fi
