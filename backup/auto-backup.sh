4e36dcf4335320e69be28bbc3baa5c1ba3c06187
Maximilian von Gaisberg <github@vongaisberg.de>
2019-05-08 14:02:18 +0200

    Add eduroam to list of acceptable wifis

--- backup/auto-backup.sh
+++ backup/auto-backup.sh
@@ -3,7 +3,7 @@
 myssid=$(iwgetid -r)
 
 
-if [ "$myssid" = "GAIS" ] || [ "$myssid" = "A304_Maximilian" ] || [ "$myssid" = "karlsruhe.freifunk.net" ] || [ "$myssid" = "KIT" ] ; then
+if [ "$myssid" = "GAIS" ] || [ "$myssid" = "eduroam" ] || [ "$myssid" = "A304_Maximilian" ] || [ "$myssid" = "karlsruhe.freifunk.net" ] || [ "$myssid" = "KIT" ] ; then
 sudo	/home/max/git/dotfiles/backup/backup.sh &
 sleep 1
 pid=$(ps aux | grep '[b]org create' | grep -v grep | awk '{print $2}')
