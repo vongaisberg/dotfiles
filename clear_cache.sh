b3aeb685b1ca1975cf0a824e0631b0fafb51b700
Maximilian von Gaisberg <git@vongaisberg.de>
2021-06-02 12:50:18 +0200

    Youtube Adblock

--- clear_cache.sh
+++ clear_cache.sh
@@ -1,2 +1,9 @@
 cargo cache -a
+# delete orphans
+yay -Rns $(yay -Qtdq)
+
+#delete pacman cache
 yay -Sc
+
+#delete old journal entries
+sudo journalctl --vacuum-time=4weeks
--- qutebrowser/adblock.js
+++ qutebrowser/adblock.js
@@ -1,10 +1,20 @@
-try{
-document.getElementsByClassName('ad-interrupting')[0].querySelector('video').currentTime = document.getElementsByClassName('ad-interrupting')[0].querySelector('video').duration;
+function skip() {
+  try {
+    let vid = document
+      .getElementsByClassName("ytp-ad-player-overlay")[0]
+      .parentElement.parentElement.getElementsByClassName(
+        "html5-video-container"
+      )[0]
+      .querySelector("video");
+    vid.currentTime = vid.duration;
+    document.getElementsByClassName("ytp-ad-skip-button")[0].click();
+  } catch {}
+  try {
+    document.getElementById("player-ads").remove();
+  } catch {}
 }
-catch{}
-try {
-document.getElementsByClassName('ytp-ad-overlay-slot')[0].style.display = 'none'}
 
-catch{}
-
-document.getElementsByClassName('ytp-ad-skip-button')[0].click()
+skip();
+setInterval(function () {
+  skip();
+}, 250);
